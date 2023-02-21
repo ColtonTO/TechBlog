418846a16e6da65cd6f75cababf6203313ed617e
ColtonTO <colton1105@gmail.com>
2023-02-21 12:01:31 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const session = require('express-session');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(session)
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
